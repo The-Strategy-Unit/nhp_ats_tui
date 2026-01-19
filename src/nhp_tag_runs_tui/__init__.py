bed146f93313bbe083e7ac75e560259c7c434ac4
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2026-01-19 15:27:15 +0000

    Add python functions, TUI call under src

---
+++ src/nhp_tag_runs_tui/__init__.py
@@ -0,0 +1,15 @@
+from .table import (
+    get_table_client,
+    get_unique_schemes,
+    fetch_scenarios,
+    list_scenarios,
+    update_entity,
+)
+
+__all__ = [
+    "get_table_client",
+    "get_unique_schemes",
+    "fetch_scenarios",
+    "list_scenarios",
+    "update_entity",
+]
