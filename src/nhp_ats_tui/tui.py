7b20031605efc7911e2ad88e300c95c9bc200637
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2026-05-07 16:07:38 +0100

    Handle keyboard interrupt

--- src/nhp_ats_tui/tui.py
+++ src/nhp_ats_tui/tui.py
@@ -37,95 +37,101 @@ def main() -> None:
 
     print("⏳ Fetching scheme codes...")
     schemes_unique = get_unique_schemes(table)
-    scheme_choice = inquirer.fuzzy(
-        message="Choose a scheme:",
-        choices=schemes_unique,
-    ).execute()
 
-    scenarios = fetch_scenarios(table, scheme_choice)
-    scenarios_list = list_scenarios(scenarios)
-
-    scenario_choice = inquirer.select(
-        message="Choose a scenario to edit:",
-        choices=scenarios_list,
-    ).execute()
-
-    task_choice = inquirer.select(
-        message="Choose a task:",
-        choices=[
-            "Edit the run stage",
-            "Edit sites (inpatients)",
-            "Edit sites (outpatients)",
-            "Edit sites (A&E)",
-        ],
-    ).execute()
-
-    if task_choice == "Edit the run stage":
-        tag_subtask_choice = inquirer.select(
-            message="Choose a run-stage option:",
-            choices=["Add/change", "Remove"],
+    try:
+        scheme_choice = inquirer.fuzzy(
+            message="Choose a scheme:",
+            choices=schemes_unique,
         ).execute()
 
-        if tag_subtask_choice == "Remove":
-            tag_choice = None  # will remove the run_stage property from the entity
+        scenarios = fetch_scenarios(table, scheme_choice)
+        scenarios_list = list_scenarios(scenarios)
 
-        if tag_subtask_choice == "Add/change":
-            tag_choice = inquirer.select(
-                message="Choose a run-stage tag:",
-                choices=[
-                    "final_report_ndg2",
-                    "final_report_ndg3",
-                    "validation_report_ndg3",
-                    "validation_report_ndg2",
-                    "Other",
-                ],
+        scenario_choice = inquirer.select(
+            message="Choose a scenario to edit:",
+            choices=scenarios_list,
+        ).execute()
+
+        task_choice = inquirer.select(
+            message="Choose a task:",
+            choices=[
+                "Edit the run stage",
+                "Edit sites (inpatients)",
+                "Edit sites (outpatients)",
+                "Edit sites (A&E)",
+            ],
+        ).execute()
+
+        if task_choice == "Edit the run stage":
+            tag_subtask_choice = inquirer.select(
+                message="Choose a run-stage option:",
+                choices=["Add/change", "Remove"],
             ).execute()
 
-            if tag_choice == "Other":
-                tag_choice = inquirer.text(
-                    "Type a run-stage tag (lowercase, underscore-separated, with NDG variant):"
+            if tag_subtask_choice == "Remove":
+                tag_choice = None  # will remove the run_stage property from the entity
+
+            if tag_subtask_choice == "Add/change":
+                tag_choice = inquirer.select(
+                    message="Choose a run-stage tag:",
+                    choices=[
+                        "final_report_ndg2",
+                        "final_report_ndg3",
+                        "validation_report_ndg3",
+                        "validation_report_ndg2",
+                        "Other",
+                    ],
                 ).execute()
 
-        update_run_stage(table, scheme_choice, scenario_choice, tag_choice)
+                if tag_choice == "Other":
+                    tag_choice = inquirer.text(
+                        "Type a run-stage tag (lowercase, underscore-separated, with NDG variant):"
+                    ).execute()
 
-        if tag_subtask_choice == "Remove":
-            print("✅ Removed run-stage tag. Exiting.")
-        else:
-            print(f"✅ Set run-stage tag to '{tag_choice}'. Exiting.")
+            update_run_stage(table, scheme_choice, scenario_choice, tag_choice)
 
-    elif "Edit sites" in task_choice:
-        entity = get_table_entity(table, scheme_choice, scenario_choice)
+            if tag_subtask_choice == "Remove":
+                print("✅ Removed run-stage tag. Exiting.")
+            else:
+                print(f"✅ Set run-stage tag to '{tag_choice}'. Exiting.")
 
-        if "inpatients" in task_choice:
-            activity_type_choice = "inpatients"
-            sites_existing = entity.get("sites_ip") or "none"
-        elif "outpatients" in task_choice:
-            activity_type_choice = "outpatients"
-            sites_existing = entity.get("sites_op") or "none"
-        elif "A&E" in task_choice:
-            activity_type_choice = "A&E"
-            sites_existing = entity.get("sites_aae") or "none"
+        elif "Edit sites" in task_choice:
+            entity = get_table_entity(table, scheme_choice, scenario_choice)
 
-        print(f"ℹ️  Current {activity_type_choice} sites: {sites_existing}")
+            if "inpatients" in task_choice:
+                activity_type_choice = "inpatients"
+                sites_existing = entity.get("sites_ip") or "none"
+            elif "outpatients" in task_choice:
+                activity_type_choice = "outpatients"
+                sites_existing = entity.get("sites_op") or "none"
+            elif "A&E" in task_choice:
+                activity_type_choice = "A&E"
+                sites_existing = entity.get("sites_aae") or "none"
 
-        sites_provided = inquirer.text(
-            "Type site codes (e.g. 'XYZ01,XYZ02', 'ALL') or leave blank to remove:"
-        ).execute()
+            print(f"ℹ️  Current {activity_type_choice} sites: {sites_existing}")
 
-        update_sites(
-            table,
-            scheme_choice,
-            scenario_choice,
-            activity_type_choice,
-            sites_provided,  # site property deleted if None
-        )
+            sites_provided = inquirer.text(
+                "Type site codes (e.g. 'XYZ01,XYZ02', 'ALL') or leave blank to remove:"
+            ).execute()
 
-        if sites_provided == "":
-            print(f"✅ Removed all {activity_type_choice} sites. Exiting.")
-        else:
-            print(
-                f"✅ Set {activity_type_choice} sites to '{sites_provided}'. Exiting."
+            update_sites(
+                table,
+                scheme_choice,
+                scenario_choice,
+                activity_type_choice,
+                sites_provided,  # site property deleted if None
             )
+
+            if sites_provided == "":
+                print(f"✅ Removed all {activity_type_choice} sites. Exiting.")
+            else:
+                print(
+                    f"✅ Set {activity_type_choice} sites to '{sites_provided}'. Exiting."
+                )
+
+    except KeyboardInterrupt:
+        print("❌ Interrupted. Exiting.")
+        return
 
 
 if __name__ == "__main__":
